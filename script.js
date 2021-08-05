79909e54e4dbd6c9ba37415ab254809cf72940c9
anthony <anthonydwan@outlook.com>
2021-08-05 15:27:37 +1000

    almost done with the details

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 // generate computer's mobve (S/P/R)
 function computerPlay() {
     let decider = Math.floor(Math.random() * 3)
-    if (decider === 0) {
-        return "Rock"
-    } else if (decider === 1) {
-        return "Scissors"
-    } else {
-        return "Paper"
+    switch(decider){
+        case 0:
+            return "Rock"
+        case 1:
+            return "Scissors"
+        case 2:
+            return "Paper"
     }
 }
 
@@ -37,68 +38,119 @@ function playCore(playerSelection, computerSelection) {
     playerSelection = processInputPrompt(playerSelection)
     if (checkDraw(playerSelection, computerSelection)) {
         return "draw"
-    } else if (checkWin(playerSelection, computerSelection)) {
+    } else if (checkWin(playerSelection, computerSelection)){
         currWinScore ++
-        showScore(currWinScore, currLoseScore)
-        return "win"
+        var result = "win"
     } else {
         currLoseScore ++
-        showScore(currWinScore, currLoseScore)
-        return "lose"
+        var result = "lose"
+    }
+    showScore(currWinScore, currLoseScore)
+    return result
+}
+
+// produce string for the round
+function getRoundEnding(result, playerSelection, computerSelection){
+    switch(result){
+        case "draw":
+            return "This is a draw.";
+        case "win":
+            return `Player +1! ${playerSelection} beats ${computerSelection}`;
+        case "lose":
+            return `Computer +1! ${computerSelection} beats ${playerSelection}`;
     }
 }
-// play a round with output strings in the end
+
+// produce string for end of the game
+function getGameEnding(){
+    if (currWinScore === 5){
+        var result = "You managed to get to 5 points first! Winner!"
+    } else if (currLoseScore === 5){
+        var result = "No! Computer got to 5 points!"
+    }
+    resultPrint(result, "#gameResult")
+}
+
+
+// play a round
 function playRound(playerSelection) {
-    playerSelection = processInputPrompt(playerSelection)
+    playerSelection = processInputPrompt(playerSelection);
     const computerSelection = computerPlay();
-    const result = playCore(playerSelection, computerSelection)
-    if (result === "draw") {
-        return "This is a draw."
-    } else if (result === "win") {
-        return `You win! ${playerSelection} beats ${computerSelection}`
-    } else {
-        return `You lose! ${computerSelection} beats ${playerSelection}`
-    }
+    showWeaponChoice(playerSelection,computerSelection)
+    const result = playCore(playerSelection, computerSelection);
+    const roundEnding = getRoundEnding(result, playerSelection, computerSelection);
+    showWinner(result)
+    resultPrint(roundEnding, "#roundResult")
+
 }
 
-// game loops for 5 times and saves the score
+// game loops until 5 points
 function game(playerSelection) {
-    let scoreCount = { "win": 0, "draw": 0, "lose": 0 }
-    while (scoreCount["win"] < 5 || scoreCount["lose"] < 5) {
-        const computerSelection = computerPlay();
-        const result = playCore(playerSelection, computerSelection)
-        scoreCount[result]++
-    }
-    console.log(scoreCount)
+    // continue to play
+    if (currWinScore < 5 && currLoseScore < 5) playRound(playerSelection)
+    // game ends
+    getGameEnding()
 }
 
-//print result 
-function resultPrint(result){
-    const container = document.querySelector("#result")
-    container.textContent = result
+//print result of the round 
+function resultPrint(result, selector){
+    const container = document.querySelector(selector);
+    container.textContent = result;
+    return
 }
-
-//plays a round when button is pressed
-const buttons = document.querySelectorAll('.buttons');
-
-buttons.forEach(button => {
-    button.addEventListener('click', function () {
-        const playerSelection = this.id
-        resultPrint(playRound(playerSelection))
-    }
-    );
-});
 
 //keeping current score
 let currWinScore = 0
 let currLoseScore = 0
 
-//change the score
+//change the score on scoreboard
 function showScore(currWinScore, currLoseScore){
-    const winScore = document.querySelector("#winScore")
-    const loseScore = document.querySelector("#loseScore")
-    winScore.textContent = currWinScore
-    loseScore.textContent = currLoseScore
+    winScore.textContent = currWinScore;
+    loseScore.textContent = currLoseScore;
+}
+
+//plays a round when button is pressed
+const buttons = document.querySelectorAll('.buttons');
+buttons.forEach(button => {
+    button.addEventListener('click', function () {
+        const playerSelection = this.id
+        game(playerSelection)
+    }
+    );
+});
+
+function addFlashTransition(div, transition){
+    div.classList.add(transition)
+    div.addEventListener('transitionend', function(){
+        this.classList.remove(transition)
+    })
+}
+function showWeaponChoice(playerSelection, computerSelection){
+    const currPlayerWeapon = document.querySelector("#playerChoiceImg");
+    const currComputerWeapon = document.querySelector("#computerChoiceImg");
+    currPlayerWeapon.src = `images/${playerSelection}.png`
+    currPlayerWeapon.classList.add("rotate")
+    currComputerWeapon.src = `images/${computerSelection}.png`
+    currComputerWeapon.classList.add("rotateflip")
+    return
+}
+
+//visually show winner
+function showWinner(result){
+    const playerBorder = document.querySelector("#playerChoiceBox");
+    const computerBorder = document.querySelector("#computerChoiceBox");
+    switch(result){
+        case "win":
+            addFlashTransition(playerBorder, "winner")
+            break;
+        case "draw":
+            addFlashTransition(playerBorder, "draw")
+            addFlashTransition(computerBorder, "draw")
+            break;
+        case "lose":
+            addFlashTransition(computerBorder, "winner")
+            break;
+    }
 }
 
 // set up the score at 0, 0 
@@ -110,5 +162,4 @@ function scoreReset(){
     var currLoseScore = 0
     showScore(currWinScore, currLoseScore)
 }
-
 
